c906e24848b78a83ae82e7897c3c744009d8bd47
hcms3 <hcmontagnini888@gmail.com>
2024-12-09 14:43:21 -0300

    fix: correção de increment no carrinho.

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -3,32 +3,36 @@ import { createContext, useEffect, useState } from "react";
 export const CartContext = createContext();
 
 export default function CartProvider({ children }) {
+  const [cartItems, setCartItems] = useState([]);
 
-    const [cartItems, setCartItems] = useState([]);
+  useEffect(() => {
+    console.log(cartItems);
+  }, [cartItems]);
 
-    useEffect(() => {
-        console.log(cartItems)
-    }, [cartItems])
-
-    const addToCart = (product) => {
-        const haveItems = cartItems.find(item => item.id === product.id);
-        if (haveItems) {
-            console.log("Elemento existente");
-            if (haveItems.id === product.id) {
-                setCartItems(cartItems.map(item => {
-                    return { ...item, quantity: haveItems.quantity + 1 }
-                }))
-            }
-        } else {
-            setCartItems([...cartItems, { ...product, quantity: 1 }])
-        }
+  const addToCart = (product) => {
+    const haveItems = cartItems.find((item) => item.id === product.id);
+    if (haveItems) {
+      setCartItems(
+        cartItems.map((cartItem) =>
+          cartItem.id === product.id
+            ? { ...cartItem, quantity: cartItem.quantity + 1 }
+            : cartItem
+        )
+      );
+    } else {
+      setCartItems([...cartItems, { ...product, quantity: 1 }]);
     }
+  };
 
-    const removeItem = (product) => {
-        const newArray = cartItems.filter(item => item.id !== product.id);
-        console.log(product.quantity)
-        setCartItems(newArray)
-    }
+  const removeItem = (product) => {
+    const newArray = cartItems.filter((item) => item.id !== product.id);
+    console.log(product.quantity);
+    setCartItems(newArray);
+  };
 
-    return <CartContext.Provider value={{ cartItems, addToCart, removeItem }} >{children}</CartContext.Provider>
+  return (
+    <CartContext.Provider value={{ cartItems, addToCart, removeItem }}>
+      {children}
+    </CartContext.Provider>
+  );
 }
